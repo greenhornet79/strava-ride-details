f882d322633ee1e2a833be18603f94b64ad3d8c8
Jeremy Green <jeremygreen@Jeremys-MacBook-Pro-2.local>
2014-03-23 14:12:04 -0600

    Update readme

--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: endocreative
 Donate link: http://endocreative.com/
 Tags: biking, strava, shortcode
 Requires at least: 3.4
-Tested up to: 3.5
-Stable tag: 1.1
+Tested up to: 3.8.1
+Stable tag: 1.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -12,9 +12,7 @@ This plugin allows you to display Strava ride details from a specific ride in yo
 
 == Description ==
 
-** This plugin no longer works due to Strava removing support for their V2 API. I'll update it when/if they open V3 of their API. **
-
-This plugin allows you to display Strava ride details from a specific ride in your posts and pages using a shortcode.
+Strava Ride Details allows you to display Strava ride details from a specific ride in your posts and pages using a shortcode. It uses v3 of Strava's API, and uses OAuth for authentication. 
 
 The details displayed by default are:
 
@@ -26,6 +24,13 @@ The details displayed by default are:
 
 Each of the details can be turned on or off. The details are displayed in an unordered list so you can style the elements however you would like using CSS.
 
+While Strava does offer their own embed widget, this plugin:
+
+* does not require an iframe
+* allows you to style the results to match your site
+* will work in responsive themes
+
+
 By default all details are display using the shortcode `[strava id="ride_id"]`, where ride_id is the string of digits at the end of the URL when viewing a single ride on Strava.
 
 If you would like to remove a detail, just set it to false in the shortcode. For example, if you would like to show only the distance, then use this shortcode:
@@ -35,7 +40,16 @@ If you would like to remove a detail, just set it to false in the shortcode. For
 == Installation ==
 
 1. Upload the folder `strava-ride-details` to the `/wp-content/plugins/` directory
-1. Activate the plugin through the 'Plugins' menu in WordPress
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+## Plugin Setup
+1. Log in to your Strava account and create an application [http://www.strava.com/developers]
+3. Make sure the redirect URI of your application is the same domain as your site
+2. Copy your client ID and client secret from your application into the Strava Ride Details settings page
+3. Click "Connect with Strava" on the settings page to generate an access token
+4. Save settings
+
+## Add to page/post
 1. Insert `[strava id="ride_id"]` in your post or page
 
 
@@ -45,6 +59,12 @@ If you would like to remove a detail, just set it to false in the shortcode. For
 2. Ride details styled using CSS
 
 == Changelog ==
+
+= 1.2 =
+* Plugin updated to use version 3 of the Strava API.
+* Added settings page
+* Added authentication with Strava via OAuth.
+* Added ability to choose either English or Metric display units in settings.
 
 = 1.0 =
 * Plugin released.
--- strava-ride-details.php
+++ strava-ride-details.php
@@ -4,12 +4,12 @@
 Plugin Name: Strava Ride Details
 Plugin URI: http://www.endocreative.com
 Description: Display Strava ride details in your pages and posts with a shortcode
-Version: 1.1
+Version: 1.2
 Author: Endo Creative
 Author URI: http://www.endocreative.com
 */
 
-/*  Copyright 2013 Endo Creative (email : info@endocreative.com)
+/*  Copyright 2014 Endo Creative (email : info@endocreative.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as 
@@ -36,12 +36,6 @@ function srd_create_options_page() {
 
 function srd_settings_page() {
 
-	$defaults = array(
-	  'access_token' => '',
-	  'units' => '',
-	);
-	$options = wp_parse_args(get_option('srd_options'), $defaults);
-
 	?>
 		<div class="wrap">
 		<h2>Strava Ride Details</h2>
@@ -58,11 +52,16 @@ add_action( 'admin_init', 'srd_admin_init' );
 
 function srd_admin_init() {
 	register_setting( 'srd_options', 'srd_options', 'srd_validate_options' );
+
 	add_settings_section( 'srd_main', 'Connect to Your Strava Account', 'srd_section_text', 'srd' );
+
+	add_settings_field( 'srd_client_id', 'Client ID', 'srd_client_id_input', 'srd', 'srd_main');
+
+	add_settings_field( 'srd_client_secret', 'Client Secret', 'srd_client_secret_input', 'srd', 'srd_main');
 
 	add_settings_field( 'srd_oauth', 'Access Token', 'srd_oauth_input', 'srd', 'srd_main');
 
-	add_settings_field( 'srd_units', 'Choose Display Units', 'srd_units_input', 'srd', 'srd_main');
+	add_settings_field( 'srd_units', 'Display Units', 'srd_units_input', 'srd', 'srd_main');
 }
 
 function srd_validate_options( $input ) {
@@ -70,51 +69,70 @@ function srd_validate_options( $input ) {
 }
 
 function srd_section_text() {
+
 	echo '<p>Click the button below. You will be sent to Strava to allow access to this plugin. After authorization, you will be redirected back to this settings page.</p>';
 	echo '<p><a href="https://www.strava.com/oauth/authorize?client_id=340&response_type=code&redirect_uri=' . home_url() . '/wp-admin/options-general.php?page=strava-ride-details/strava-ride-details.php&approval_prompt=force"><img src="' . plugins_url( 'images/ConnectWithStrava.png', __FILE__) . '"></a></p>';
 
 }
 
 function srd_oauth_input() {
-	
-	$options = get_option( 'srd_options' );
+
+	$options = wp_parse_args( get_option( 'srd_options' ), array('srd_oauth' => ''));
 	$access_token = $options['srd_oauth'];
 
 	// get access token from Strava
 	if ( empty($access_token) ) {
 
-		$code = $_GET['code'];
-		$client_id = '340';
-		$client_secret = '954d86c5c4cc153ef676b4bff7f87eb711745247';
-		
-		$curl = curl_init();
+		if ( isset($_GET['code']) ) {
 
-		curl_setopt_array($curl, array(
-			CURLOPT_RETURNTRANSFER => 1,
-			CURLOPT_FOLLOWLOCATION => 1,
-			CURLOPT_POST => 1,
-			CURLOPT_URL => 'https://www.strava.com/oauth/token',
-			CURLOPT_POSTFIELDS => array(
-				client_id => $client_id,
-				client_secret => $client_secret,
-				code => $code
-			)
+			$code = $_GET['code'];
+			$client_id = $options['srd_client_id'];
+			$client_secret = $options['srd_client_secret'];
+			
+			$curl = curl_init();
 
-		));
+			curl_setopt_array($curl, array(
+				CURLOPT_RETURNTRANSFER => 1,
+				CURLOPT_FOLLOWLOCATION => 1,
+				CURLOPT_POST => 1,
+				CURLOPT_URL => 'https://www.strava.com/oauth/token',
+				CURLOPT_POSTFIELDS => array(
+					'client_id' => $client_id,
+					'client_secret' => $client_secret,
+					'code' => $code
+				)
 
-		$resp = curl_exec($curl);
-		curl_close($curl);
-		$resp = json_decode($resp);
-		$access_token = $resp->access_token;
+			));
+
+			$resp = curl_exec($curl);
+			curl_close($curl);
+			$resp = json_decode($resp);
+			$access_token = $resp->access_token;
+		}
 	}
 
 	echo "<input id='srd_oauth' name='srd_options[srd_oauth]' value='$access_token'>";
 }
 
+function srd_client_id_input() {
+
+	$options = wp_parse_args( get_option( 'srd_options' ), array('srd_client_id' => ''));
+	$client_id = $options['srd_client_id'];
+	echo "<input id='srd_client_id' name='srd_options[srd_client_id]' value='$client_id'>";
+}
+
+function srd_client_secret_input() {
+
+	$options = wp_parse_args( get_option( 'srd_options' ), array('srd_client_secret' => ''));
+	$client_secret = $options['srd_client_secret'];
+	echo "<input id='srd_client_secret' name='srd_options[srd_client_secret]' value='$client_secret'>";
+}
+
 
 function srd_units_input() {
 	
-	$options = get_option( 'srd_options' );
+	$options = wp_parse_args( get_option( 'srd_options' ), array('srd_units' => ''));
+	
 	$units = $options['srd_units'];
 	
 	echo '<select name="srd_options[srd_units]">';
@@ -195,10 +213,6 @@ function srd_get_activity( $id ) {
 	if ( empty( $activity ) )
 		return false;
 
-	// echo '<pre>';
-	// 	print_r($activity);
-	// echo '</pre>';
-
 	return array(
 		'name' => $activity->name,
 		'distance' => $activity->distance,
@@ -209,14 +223,32 @@ function srd_get_activity( $id ) {
 }
 
 function srd_ride_distance( $dis_meters ) {
-	$dis_miles = $dis_meters * .000621371;
-	return '<li class="srd-distance"><span>Distance:</span> ' . round( $dis_miles, 2) . ' miles</li>';
+
+	$options = get_option( 'srd_options' );
+	$units = $options['srd_units'];
+
+	if ( $units == 'english' ) {
+		$total_distance = round( $dis_meters * .000621371, 2 ) . ' mi';
+	} else {
+		$total_distance = round( $dis_meters * .001, 2 ) . ' km';
+	}
+	
+	return '<li class="srd-distance"><span>Distance:</span> ' . $total_distance . '</li>';
 }
 
 
 function srd_ride_elevation( $elev_meters ) {
-	$elev_ft = $elev_meters * 3.28084;
-	return '<li class="srd-elevation"><span>Elevation Gain:</span> ' . round( $elev_ft ) . ' ft</li>';
+
+	$options = get_option( 'srd_options' );
+	$units = $options['srd_units'];
+
+	if ( $units == 'english' ) {
+		$elevation = round( $elev_meters * 3.28084, 2 ) . ' ft';
+	} else {
+		$elevation = round( $elev_meters, 2) . ' m';
+	}
+
+	return '<li class="srd-elevation"><span>Elevation Gain:</span> ' . $elevation . '</li>';
 }
 
 
